1a4492d5bc762c5fc28e06ef02042a2e94d30a87
prosper <edwardprosper001@gmail.com>
2023-02-27 20:56:55 +0100

    updates

--- orders/forms.py
+++ orders/forms.py
@@ -7,6 +7,7 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
                   'postal_code', 'city']
+
         
 
 
--- users/forms.py
+++ users/forms.py
@@ -36,7 +36,7 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['phone_number', 'image']
 
 
 
